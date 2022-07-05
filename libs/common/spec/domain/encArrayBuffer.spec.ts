56a645bec528ce3ff85d3a8070c4a496f9ae68f2
Thomas Rittson <trittson@bitwarden.com>
2022-07-05 14:00:54 +1000

    Use custom matcher in encArrayBuffer tests

--- libs/common/spec/domain/encArrayBuffer.spec.ts
+++ libs/common/spec/domain/encArrayBuffer.spec.ts
@@ -19,16 +19,12 @@ describe("encArrayBuffer", () => {
       array.set(mac, 1 + iv.byteLength);
       array.set(cipherText, 1 + iv.byteLength + mac.byteLength);
 
-      // Note: comparing 2 ArrayBuffers will always pass, need to wrap in Uint8Arrays
       const actual = new EncArrayBuffer(array.buffer);
-      const actualIv = new Uint8Array(actual.ivBytes);
-      const actualMac = new Uint8Array(actual.macBytes);
-      const actualCipherText = new Uint8Array(actual.ctBytes);
 
       expect(actual.encryptionType).toEqual(encType);
-      expect(actualIv).toEqual(iv);
-      expect(actualMac).toEqual(mac);
-      expect(actualCipherText).toEqual(cipherText);
+      expect(actual.ivBytes).isBufferEqualTo(iv);
+      expect(actual.macBytes).isBufferEqualTo(mac);
+      expect(actual.ctBytes).isBufferEqualTo(cipherText);
     });
 
     it("with AesCbc256_B64", () => {
@@ -41,14 +37,11 @@ describe("encArrayBuffer", () => {
       array.set(iv, 1);
       array.set(cipherText, 1 + iv.byteLength);
 
-      // Note: comparing 2 ArrayBuffers will always pass, need to wrap in Uint8Arrays
       const actual = new EncArrayBuffer(array.buffer);
-      const actualIv = new Uint8Array(actual.ivBytes);
-      const actualCipherText = new Uint8Array(actual.ctBytes);
 
       expect(actual.encryptionType).toEqual(encType);
-      expect(actualIv).toEqual(iv);
-      expect(actualCipherText).toEqual(cipherText);
+      expect(actual.ivBytes).isBufferEqualTo(iv);
+      expect(actual.ctBytes).isBufferEqualTo(cipherText);
       expect(actual.macBytes).toBeNull();
     });
   });
